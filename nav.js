9e347c0db1ae6c4639c7bb31e0fc21510b8c6173
vinicius.marques <msvinicius27@gmail.com>
2024-03-13 21:18:14 -0300

    adicao dos arquivos

---
+++ nav.js
@@ -0,0 +1,13 @@
+// Carrega a barra de navegação em todas as páginas
+document.addEventListener("DOMContentLoaded", function () {
+    const navContainer = document.createElement('div');
+    navContainer.setAttribute('id', 'nav-container');
+
+    // Fetch nav.html and include it
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(html => {
+            navContainer.innerHTML = html;
+            document.body.prepend(navContainer);
+        });
+});
